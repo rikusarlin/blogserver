0804c0d3230f9d08d5524793b4534ed0d68f3abd
Riku Sarlin <Riku@MacBook-Pro.local>
2020-09-12 17:36:18 +0300

    added production UI

---
+++ build/precache-manifest.f4c3f50c408f753a6403e37fb389968a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12529f5c64d2bf1376394166304ac05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2903bf4f3e6b4579c7",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6128c6453567ae315575",
+    "url": "/static/css/main.3049d4a3.chunk.css"
+  },
+  {
+    "revision": "3d2903bf4f3e6b4579c7",
+    "url": "/static/js/2.550a55fd.chunk.js"
+  },
+  {
+    "revision": "2f71fbc01fec0c62d6e32185bc021541",
+    "url": "/static/js/2.550a55fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6128c6453567ae315575",
+    "url": "/static/js/main.7f2c0ada.chunk.js"
+  },
+  {
+    "revision": "038c38d3471e45bc1ed6",
+    "url": "/static/js/runtime-main.63d0d90a.js"
+  }
+]);
